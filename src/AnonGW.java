0cc6414359b8796c4b0d013de63f10190e857328
Luís Macedo <luisjrsm@hotmail.com>
2020-04-20 12:01:07 +0100

    asd

--- src/AnonGW.java
+++ src/AnonGW.java
@@ -1,12 +1,9 @@
-import com.sun.source.tree.NewArrayTree;
-
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.DatagramPacket;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class AnonGW {
@@ -65,6 +62,42 @@ public class AnonGW {
         return sb.toString();
     }
 
+    public static void readFromClient(InputStream client_in, byte[] buff) throws IOException {
+        int clientInCount = 0;
+        System.out.println("Ler a Mensagem\n");
+        while ((clientInCount = client_in.read(buff)) > 0){
+            if(clientInCount < 4098){
+                break;
+            }
+        }
+        System.out.println("Mensagem lida\n");
+    }
+
+    public static void sendToClient(OutputStream client_out, byte[] buff) throws IOException {
+        System.out.println("Enviar resposta\n");
+        client_out.write(buff);
+        client_out.flush();
+        System.out.println("Resposta enviada\n");
+    }
+
+    public static void readFromTarget(InputStream target_in, byte[] buff) throws IOException {
+        int targetInCount = 0;
+        System.out.println("Receber resposta\n");
+        while((targetInCount = target_in.read(buff)) > 0){
+            if(targetInCount < 4098){
+                break;
+            }
+        }
+        System.out.println("Resposta lida\n");
+    }
+
+    public static void sendToTarget(OutputStream target_out, byte[] buff) throws IOException {
+        System.out.println("Enviar a mensagem\n");
+        target_out.write(buff);
+        target_out.flush();
+        System.out.println("Mensagem Enviada\n");
+    }
+
     public static void main(String[] args) {
         boolean error = false;
         int currentParams = 0;
@@ -113,18 +146,25 @@ public class AnonGW {
             ServerSocket ss = new ServerSocket(me.getPort());
 
             while(true){
-                //receber pedido do cliente
-                Socket client = ss.accept();
-                InputStream in = client.getInputStream();
-                int count;
-                byte[] bufIn = new byte[1024];
-                while ((count = in.read(bufIn)) > 0);
+                byte[] buff;
 
-                //enviar pedido para o targetServer
+                //Cliente
+                Socket client = ss.accept();
+                InputStream client_in = client.getInputStream();
+                OutputStream client_out = client.getOutputStream();
+
+                //targetServer
                 Socket target = new Socket(me.getTargetServer(),me.getPort());
-                OutputStream out = target.getOutputStream();
-                byte[] bufOut = Arrays.copyOf(bufIn,bufIn.length);
-                out.write(bufOut);
+                InputStream target_in = target.getInputStream();
+                OutputStream target_out = target.getOutputStream();
+
+                buff = new byte[4098];
+                readFromClient(client_in, buff);
+                sendToTarget(target_out, buff);
+                buff = new byte[4098];
+                readFromTarget(target_in, buff);
+                sendToClient(client_out, buff);
+
                 client.close();
             }
         }
